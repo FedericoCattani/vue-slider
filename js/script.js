b58ea4dd2da387f33e50c472b71699c7b711f0c3
FedericoCattani <federico.cattani1@gmail.com>
2023-05-01 16:26:36 +0200

    slider

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
-const {createApp} = Vue; 
+const { createApp } = Vue;
 
 
 createApp({
 
 
-  data(){
-    return{
-      
+  data() {
+    return {
+      posters: [
+        "01.webp",
+        "02.webp",
+        "03.webp",
+        "04.webp",
+        "05.webp"
+      ],
+      counter: 0
 
     }
+  },
+
+  methods: {
+    nextPrev(isNext) {
+      isNext ? this.counter++ : this.counter--;
+
+      if (this.counter === this.posters.length) this.counter = 0;
+      if (this.counter < 0) this.counter = this.posters.length - 1;
+    },
+
+    startAutoscroll() {
+      setInterval(() => {
+        this.nextPrev(true)
+      }, 2000)
+    }
+  },
+
+  mounted() {
+    this.startAutoscroll()
   }
 
-})
+}).mount('#app')
